820bf8ede0bbd9ae717dda916b931485688fcef5
profsaz <sazprof@gmail.com>
2024-03-16 16:39:13 +0100

    interactions added

---
+++ script/Interactiosn.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract Interactions is Script{
+    function FundFundMe() public {}
+
+    function WithdrawFundMe() public {}
+}
