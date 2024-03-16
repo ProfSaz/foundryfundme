29e393641438e04fcc0eadc2a6fe50827ab86455
profsaz <sazprof@gmail.com>
2024-03-16 16:57:36 +0100

    functions defined

--- script/Interactiosn.s.sol
+++ script/Interactiosn.s.sol
@@ -2,9 +2,25 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/Fundme.sol";
 
 contract Interactions is Script{
-    function FundFundMe() public {}
 
-    function WithdrawFundMe() public {}
+    uint256 constant public SEND_VALUE = 0.01 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+
+        console.log("funded with %s", SEND_VALUE);
+
+
+    }
+
+    function run() external  {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+    }
+    function withdrawFundMe() public {}
 }
