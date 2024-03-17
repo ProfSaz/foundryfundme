e49c03cf0b4b7ceddb834f724147f6ba1f7ad93a
profsaz <sazprof@gmail.com>
2024-03-17 11:31:25 +0100

    Unit test initialized

--- test/units/FundMeTest.t.sol
+++ test/units/FundMeTest.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.18;
 
 import {Test,console} from "forge-std/Test.sol";
-import {FundMe} from "../src/Fundme.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/Fundme.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 
 contract FundMeTest is StdCheats, Test {
